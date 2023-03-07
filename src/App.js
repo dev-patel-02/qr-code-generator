9b1094266cd627b5cd10f556052c6ca67884fef9
Dev Patel <dev220791@gmail.com>
2023-03-07 11:25:28 +0600

    axe-core implement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./App.css";
 import Card from "./components/Card";
+import { axe, ReactAxe } from "@axe-core/react";
 
 function App() {
   return (
-    <div className="container mx-auto flex justify-center items-center h-screen px-4">
-      <Card />
-    </div>
+      <div className="container mx-auto flex justify-center items-center h-screen px-4">
+        <main></main>
+        <Card />
+      </div>
   );
 }
 
 export default App;
+if (process.env.NODE_ENV !== "production") {
+  const axe = require("@axe-core/react");
+  axe(React, ReactDOM, 1000);
+}
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,22 @@ import code from "../images/code.png";
 function Card() {
   return (
     <div className="bg-white w-80 rounded-3xl p-4 shadow-xl">
-      <img className="rounded-xl" src={code} alt="code" />
-      <p className="text-xl font-bold px-4 py-4 text-center">
-        Improve your front-end skills by building projects
-      </p>
-      <p className="text-sm text-gray-600 font-medium px-6 pb-5  text-center">
-        Scan the QR code to visit Fronted mentor and take your coding skills to
-        the next level
-      </p>
+      <div role="banner">
+        <img 
+          className="rounded-xl w-[300px] h-[288px]"
+          src={code}
+          alt="bar-code"
+        />
+      </div>
+      <div role="contentinfo">
+        <h1 className="text-xl font-bold px-4 py-4 text-center">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-sm text-gray-600 font-medium px-6 pb-5  text-center">
+          Scan the QR code to visit Fronted mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
     </div>
   );
 }
