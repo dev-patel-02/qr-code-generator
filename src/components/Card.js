16e37a679f0e91e0508c3d3b5bf4e78501c27203
Dev Patel <dev220791@gmail.com>
2023-03-09 14:54:07 +0600

    axe-core problem solve

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,15 @@ import code from "../images/code.png";
 function Card() {
   return (
     <div className="bg-white w-80 rounded-3xl p-4 shadow-xl">
-      <div role="banner">
-        <img 
+      <div className="banner">
+      <img 
           className="rounded-xl w-[300px] h-[288px]"
           src={code}
           alt="bar-code"
         />
       </div>
-      <div role="contentinfo">
+      
+      <div className="contentinfo">
         <h1 className="text-xl font-bold px-4 py-4 text-center">
           Improve your front-end skills by building projects
         </h1>
